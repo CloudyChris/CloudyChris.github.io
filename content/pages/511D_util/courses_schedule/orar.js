733d2d2e4c5cfc549dea6e05a638b9d19dce49eb
CloudyChris <damianfate@gmail.com>
2017-10-11 23:47:34 +0300

    update to downloads - 511D

--- content/pages/511D_util/courses_schedule/orar.js
+++ content/pages/511D_util/courses_schedule/orar.js
@@ -1,24 +1,24 @@
 
 function gen_schedule(schxml, did){
-	
+
 	var sch_ul = document.createElement("ul");
-	
+
 	$(schxml).find("ent").each(function(){
 			var sch_ent = document.createElement("li");
-            
+
 		})
 }
 
 function gen_day(dxml){
     var tab_li = document.createElement("li");
     var tab_a  = document.createElement("a");
-    
+
     var dname = document.createTextNode($(dxml).find("day").text());
-    
+
     tab_a.appendChild(dname);
-    
+
     tab_li.appendChild(tab_a);
-    
+
     gen_schedule($(dxml).find("sch"), $(dxml).find("did").text());
 }
 
@@ -29,7 +29,7 @@ $(document).ready(function(){
 		 var $main_xml = $( $.parseXML(response));
          $(main_xml).find("orar > zi").each(function(){
 			gen_day($(this));
-            
+
 		})
 		 //$("#version_ind").append($main_xml.find("content > version").text());
 	});

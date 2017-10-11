56650a24f4dc0a95975700384ba20123e04f0e1b
Pufu Andrei Cristian <damianfate@gmail.com>
2017-10-11 12:31:01 +0000

    shcedule tabs gen function update

--- content/pages/511D_util/courses_schedule/orar.js
+++ content/pages/511D_util/courses_schedule/orar.js
@@ -1,11 +1,23 @@
+
+function gen_day(dxml){
+    var tab_li = document.createElement("li");
+    var tab_a  = document.createElement("a");
+    
+    var dname = document.createTextNode($dxml.find("day").text());
+    
+    tab_a.appendChild(dname);
+    
+    tab_li.appendChild(tab_a);
+}
+
 $(document).ready(function(){
 
     $.get( "content/pages/511D_util/courses_schedule/orar.xml", function(response) {
 
 		 var $main_xml = $( $.parseXML(response));
          $(main_xml).find("orar > zi").each(function(){
-			$("#orar_container").empty();
-
+			gen_day($(this));
+            
 		})
 		 //$("#version_ind").append($main_xml.find("content > version").text());
 	});
